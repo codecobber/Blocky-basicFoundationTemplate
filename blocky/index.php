d44241c17c6e68d1780dbe790132a62d61194041
Cobber <codecobber@gmail.com>
2018-02-09 11:33:15 +0000

    Add files via upload

---
+++ blocky/index.php
@@ -0,0 +1,64 @@
+<?php include('includes/header.inc.php'); ?><div id='start'>
+
+
+       <div id="bck_1" class="bck"><div id="block_1" class="row rowNo05">
+
+<div class="medium-6 columns medium-push-6">
+<img class="imgPlace" src="http://placehold.it/750x350">
+</div>
+<div class="medium-6 columns medium-pull-6 ">
+<h2>Our Agency, our selves.</h2>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+</div>
+
+</div></div><div id="bck_2" class="bck"><div id="block_2" class="row rowNo10">
+<div class="medium-6 columns medium-6">
+<img class="imgPlace thumbnail" src="http://placehold.it/750x350">
+</div>
+<div class="medium-6 columns medium-6">
+<h2>Our Agency, our selves.</h2>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+</div>
+</div></div><div id="bck_3" class="bck"><div id="block_3" class="row rowNo05">
+
+<div class="medium-6 columns medium-push-6">
+<img class="imgPlace" src="http://placehold.it/750x350">
+</div>
+<div class="medium-6 columns medium-pull-6 ">
+<h2>Our Agency, our selves.</h2>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+</div>
+
+</div></div><div id="bck_4" class="bck"><div id="block_4" class="row rowNo10">
+<div class="medium-6 columns medium-6">
+<img class="imgPlace thumbnail" src="http://placehold.it/750x350">
+</div>
+<div class="medium-6 columns medium-6">
+<h2>Our Agency, our selves.</h2>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+</div>
+</div></div><div id="bck_5" class="bck"><div id="block_5" class="row rowNo19">        <div class="large-9 large-centered columns">
+            <p class="text-center">Lorem ipsum dolor sit amet, nulla cras penatibus sodales nunc vestibulum ut, maecenas suscipit, mi sem dolor leo wisi id, leo vitae eget posuere purus aliquam morbi. Vulputate fusce sapien non, tristique in non amet ac vitae tristique, aliquam vivamus a, ipsum malesuada sagittis nulla, id praesent morbi rhoncus morbi luctus. Ligula odio enim sed nisl diam urna.
+          </p>
+       </div>
+</div></div><div id="bck_6" class="bck"><div id="block_6" class="row small-up-1 medium-up-2 large-up-3 rowNo39"><div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div>
+<div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div>
+<div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div></div></div></div><?php include('includes/footer.inc.php'); ?>
